68094fbc8e4807551b0a94082ffd36e67809d620
Jocelyn Bohr <bjocelyn@loadtestp04.dc.umich.edu>
2014-10-25 19:47:51 -0400

    put data in one file, boxes.mat

--- histogram.m
+++ histogram.m
@@ -1,7 +1,7 @@
-function [ hist ] = histogram( pts, file )
+function [ hist_mat ] = histogram( pts, file )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-    hist = zeros(64,64,64);
+    hist_mat = zeros(64,64,64);
     x_min = min(pts(1,1), pts(2,1));
     x_max = max(pts(1,1), pts(2,1));
     y_min = min(pts(1,2), pts(2,2));
@@ -10,7 +10,7 @@ function [ hist ] = histogram( pts, file )
     for i=x_min:x_max
        for j=y_min:y_max
            RGB = A(int64(j),int64(i),:);
-           hist(floor(RGB(:,:,1)/4), floor(RGB(:,:,2)/4), floor(RGB(:,:,3)/4)) = hist(floor(RGB(:,:,1)/4), floor(RGB(:,:,2)/4), floor(RGB(:,:,3)/4)) + 1;
+           hist_mat(floor(RGB(:,:,1)/4), floor(RGB(:,:,2)/4), floor(RGB(:,:,3)/4)) = hist_mat(floor(RGB(:,:,1)/4), floor(RGB(:,:,2)/4), floor(RGB(:,:,3)/4)) + 1;
        end
     end
 end
