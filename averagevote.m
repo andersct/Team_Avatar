5e88b6ce88509689ffdf2c964150c585bb6e14ae
sgtrouge <nghiavo@0587354754.wireless.umich.net>
2014-10-30 22:11:40 -0400

    dumb algo

---
+++ averagevote.m
@@ -0,0 +1,43 @@
+clear;
+load('data.mat');
+perm = randperm(830);
+train_t_split = train_t(perm(1:700));
+train_x_split = train_x(perm(1:700),:);
+test_x_split = train_x(perm(701:end),:);
+test_t_split = train_t(perm(701:end));
+a = zeros(3,1);
+color = zeros(6,3);
+color(1,:) = [64,1,1];
+color(2,:) = [1,64,1];
+color(3,:) = [64,64,64];
+color(4,:) = [1,1,64];
+color(5,:) = [64,64,1];
+color(6,:) = [1,1,1];
+cor = 0;
+rescolor = zeros(130,1);
+for i=1:130
+    [x,y] = max(test_x_split(i,:));
+    ind = 1;
+    a = zeros(3,1);
+    count = 0;
+    for rc = 1:64
+        for gc = 1:64
+            for bc = 1:64
+                a(1) = a(1) + test_x_split(i,ind)*rc;
+                a(2) = a(2) + test_x_split(i,ind)*gc;
+                a(3) = a(3) + test_x_split(i,ind)*bc;
+                count = count + test_x_split(i,ind);
+                ind = ind + 1;
+            end;
+        end;
+    end;
+    a = a/count;
+    for j = 1:6
+        d(j) = (a - color(j,:)')'*(a-color(j,:)');
+    end;
+    [resind,rescolor(i)] = min(d);
+    if (rescolor(i) == test_t_split(i)) 
+        cor = cor+1;
+    end;
+end;
+cor/130
---
+++ majorityvote.m
@@ -0,0 +1,33 @@
+clear;
+load('data.mat');
+perm = randperm(830);
+train_t_split = train_t(perm(1:700));
+train_x_split = train_x(perm(1:700),:);
+test_x_split = train_x(perm(701:end),:);
+test_t_split = train_t(perm(701:end));
+a = zeros(3,1);
+color = zeros(6,3);
+color(1,:) = [63,0,0]; %red
+color(2,:) = [0,63,0]; %green
+color(3,:) = [63,63,63]; 
+color(4,:) = [0,0,63];
+color(5,:) = [63,63,0];
+color(6,:) = [0,0,0];
+cor = 0;
+resrgb = zeros(130,3);
+rescolor = zeros(130,1);
+for i=1:130
+    [x,y] = max(test_x_split(i,:));
+    a(1) = y/(64*64);
+    a(2) = mod(y,(64*64)) / 64;
+    a(3) = mod(y,64);
+    for j = 1:6
+        d(j) = (a - color(j,:)')'*(a-color(j,:)');
+    end;
+    [resind,rescolor(i)] = min(d);
+    if (rescolor(i) == test_t_split(i)) 
+        cor = cor+1;
+    end;
+    resrgb(i,:) = a';
+end;
+cor/130
