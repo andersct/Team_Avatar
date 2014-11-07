614f7a40636545fe18ad331503ff39842789865a
andersct <andersct@umich.edu>
2014-11-07 10:27:08 -0500

    added color of box to visualize function, related work section in report

--- visualize.m
+++ visualize.m
@@ -17,7 +17,7 @@ title(sprintf('Image %.0f, %s', index, pictureDataFixed{index,3}));
 
 hold on;
 %actually get color
-[r, g, b] = colorTextToRgb('white');
+[r, g, b] = colorTextToRgb(colorToText(pictureDataFixed{index,2}));
 h(1) = plot([x(1), x(1)], [y(1), y(2)], 'Color', [r, g, b], 'LineWidth', 2);
 h(2) = plot([x(2), x(2)], [y(1), y(2)], 'Color', [r, g, b], 'LineWidth', 2);
 h(3) = plot([x(1), x(2)], [y(1), y(1)], 'Color', [r, g, b], 'LineWidth', 2);
