4c91834192b90d0edf80655e048c1bcf30d80a3c
Jocelyn Bohr <bjocelyn@caen-vnc17.engin.umich.edu>
2014-12-02 16:17:02 -0500

    added lots of stuff, fixed svm and naive bayes issues. added weighted svm. may need to make new libsvm folder for mac users

--- nb_script.m
+++ nb_script.m
@@ -1,25 +1,14 @@
-load('data.mat');
-perm = randperm(830);
-test_x = train_x(perm(701:end), :);
-test_t = train_t(perm(701:end));
-train_x_split = train_x(perm(1:700), :);
-train_t_split = train_t(perm(1:700));
+load('datanb.mat');
+N = length(train_t);
+split = 1700;
+perm = randperm(N);
+train_x = train_x(perm,:);
+train_t = train_t(perm);
+test_x = train_x(split+1:end, :);
+test_t = train_t(split+1:end);
+train_x_split = train_x(1:split, :);
+train_t_split = train_t(1:split);
 
-% for binary classification
-%for i=1:130
-%    if (test_t(i) ~= 6)
-%        test_t(i) = 0;
-%    else
-%        test_t(i) = 1;
-%    end
-%end
-%for i=1:700
-%    if (train_t_split(i) ~= 6)
-%        train_t_split(i) = 0;
-%    else
-%        train_t_split(i) = 1;
-%    end
-%end
 nb = fitNaiveBayes(train_x_split, train_t_split, 'Distribution', 'mn');
 pred = predict(nb,test_x);
 
