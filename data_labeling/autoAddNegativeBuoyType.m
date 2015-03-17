205034fff0457ef2e8d2cd7cdcd0cc29fc3ade2f
andersct <andersct@umich.edu>
2015-03-17 17:41:35 -0400

    labeled negatives as negative-type buoys

--- data_labeling/autoAddNegativeBuoyType.m
+++ data_labeling/autoAddNegativeBuoyType.m
@@ -3,7 +3,7 @@ function autoAddNegativeBuoyType(pictureData, saveDir)
 %   Detailed explanation goes here
 
 negativeLabel = buoyTypeToLabel('negative');
-index = 4;
+index = 4; % index of feature - 4 for buoy type: tall|sphere|negative
 numImages = length(pictureData);
 for i=1:numImages
     numObjects = length(pictureData{i, 2});
