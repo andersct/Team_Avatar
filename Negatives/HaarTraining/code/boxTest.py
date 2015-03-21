84cf4ce02c080b4101e0de92458b2aa1f0e06b0b
andersct <andersct@umich.edu>
2015-03-20 21:40:44 -0400

    fixed bugs, added features to evaluator

--- Negatives/HaarTraining/code/boxTest.py
+++ Negatives/HaarTraining/code/boxTest.py
@@ -1,9 +1,11 @@
 import box_test_helper
 from itertools import izip
 
-training = 'training.txt'
-predictions = 'predictions.txt'
-MIN_OVERLAP = 0.5
+training = 'info.txt'
+predictions = 'predictions.txt' # 'predictionsGrayCutoff.txt'  #  'predictionsColor.txt'  # 'predictions.txt'
+repo = '/Users/CyrusAnderson/Documents/Team_Avatar/'
+MIN_OVERLAP = 0.4  # 0.5 was a bit too high. 0.4 is slightly lenient
+show_results = True
 
 tp = 0
 fp = 0
@@ -14,15 +16,21 @@ with open(training, 'r') as training_file, open(predictions, 'r') as pred_file:
 
         labels = truth.strip('\n\r').split(' ')
         preds = pred.strip('\n\r').split(' ')
+        # make sure first arg matches? [link, number of boxes, [box1], [box2], ...
         print(labels)
+        # [link, [box1], [box2], ...
         print(preds)
-        # make sure first arg matches? [link, [box1], [box2], ...
 
-        img_tp, img_fp, img_fn, boxes = box_test_helper.evaluate_predictions(labels[1:], preds[1:], MIN_OVERLAP)
+        img_tp, img_fp, img_fn, boxes, matches = \
+            box_test_helper.evaluate_predictions(labels[2:], preds[1:], MIN_OVERLAP)
         print(img_tp, img_fp, img_fn, boxes)
         tp += img_tp
         fp += img_fp
         fn += img_fn
         total += boxes
 
+        if show_results:
+            box_test_helper.plot_matches(repo+labels[0], map(int, matches),
+                                         map(int, labels[2:]), map(int, preds[1:]), True)
 
+print(total, tp, fp, fn)
--- Negatives/HaarTraining/code/box_test_helper.py
+++ Negatives/HaarTraining/code/box_test_helper.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib.patches import Rectangle
+
+
 # works for test and train data
 def evaluate_predictions(labels, preds, min_dist):
     """
@@ -9,6 +14,7 @@ def evaluate_predictions(labels, preds, min_dist):
     num_real = len(labels)/4
 
     matches = [-1] * num_real
+    mapping = [-1] * num_real
     fp = 0
     for i in range(0, num_preds):
         # for all of preds, find overlap with each real and vote for the best real box
@@ -23,7 +29,7 @@ def evaluate_predictions(labels, preds, min_dist):
         scores = [-1] * num_real
         for j in range(0, num_real):
             print('i, j: ' + str(i) + ' ' + str(j))
-            scores[j] = box_dist(map(int, preds[4*i: 4+4*i]), map(int, labels[4*j: 4+4*j]))
+            scores[j] = box_dist(map(int, get_box(preds, i)), map(int, get_box(labels, j)))
         best = max(scores)
         other = matches[scores.index(best)]
         if best < min_dist or best < other:
@@ -31,12 +37,17 @@ def evaluate_predictions(labels, preds, min_dist):
         elif other != -1:
             fp += 1
             matches[scores.index(best)] = best
+            mapping[scores.index(best)] = i
         else:
             matches[scores.index(best)] = best
+            mapping[scores.index(best)] = i
     fn = sum(x < 0 for x in matches)
-    tp = sum(x > 0 for x in matches)
+    tp = sum(x > -1 for x in matches)
 
-    return tp, fp, fn, num_real
+    print('stats')
+    print(mapping)
+    print(matches)
+    return tp, fp, fn, num_real, mapping
 
 
 def box_dist(b1, b2):
@@ -63,3 +74,73 @@ def box_dist(b1, b2):
     return iw*ih/float(ua)
 
 
+def plot_matches(link, match, labels, preds, show_all_preds):
+    """
+    Plots labels and predictions and opens the image in a window, pausing until closed.
+    :param link:    filepath to image
+    :param match:   list of preds matching labels: match[i] = j if pred j matches label i
+                    -1 if no match, as ints
+    :param labels: ground truth in [left x, top y, w, h, ... as ints
+    :param preds: predictions in [left x, top y, w, h, ... as ints
+    :param show_all_preds: shows all predictions iff true
+    :return:
+    """
+    im = mpimg.imread(link)
+    plt.imshow(im)
+    if show_all_preds:
+        plot_all_predictions(match, labels, preds)
+    else:
+        plot_matching_predictions(match, labels, preds)
+    plt.show()
+
+
+def plot_matching_predictions(match, labels, preds):
+    """
+    Plots all matching predictions.
+    :param match:   list of preds matching labels: match[i] = j if pred j matches label i
+                    -1 if no match, as ints
+    :param labels: ground truth in [left x, top y, w, h, ... as ints
+    :param preds: predictions in [left x, top y, w, h, ... as ints
+    """
+    for i, m in enumerate(match):
+        # plot label in pink and its match in orange
+        add_box(get_box(labels, i), 'pink')
+        if m == -1:
+            continue
+        add_box(get_box(preds, m), 'orange')
+
+
+def plot_all_predictions(match, labels, preds):
+    """
+    Plots all predictions.
+    :param match:   list of preds matching labels: match[i] = j if pred j matches label i
+                    -1 if no match, as ints
+    :param labels: ground truth in [left x, top y, w, h, ... as ints
+    :param preds: predictions in [left x, top y, w, h, ... as ints
+    """
+
+    num_real = len(labels)/4
+    for i in range(0, num_real):
+        add_box(get_box(labels, i), 'pink')
+    num_preds = len(preds)/4
+    for i in range(0, num_preds):
+        if i in match:
+            add_box(get_box(preds, i), 'purple')
+        else:
+            add_box(get_box(preds, i), 'orange')
+
+
+def add_box(b, edge_color):
+    """
+    Adds a box to the given figure.
+    :param b:           [left x, top y, w, h] as ints
+    :param edge_color:  string
+    :return:
+    """
+    current_axis = plt.gca()
+    current_axis.add_patch(Rectangle(b[:2], b[2], b[3], facecolor='none', edgecolor=edge_color))
+    print(b[:2], b[2], b[3])
+
+
+def get_box(box_list, i):
+    return box_list[4*i: 4+4*i]
