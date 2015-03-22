92bd056524ca68911aa1fba4b88dfdd659df003f
cyrus anderson <cyrusanderson@cyruss-Mac-mini.local>
2015-03-22 11:19:10 -0400

    training results

--- Negatives/HaarTraining/code/Predict.cpp
+++ Negatives/HaarTraining/code/Predict.cpp
@@ -14,7 +14,7 @@ using namespace cv;
 void writePrediction(Mat frame, String link);
 
 /** Global variables */
-String cascade_name = "Negatives/HaarTraining/code/cascade.xml";
+String cascade_name = "Negatives/HaarTraining/model/cascade.xml";
 String repo_dir = "/Users/cyrusanderson/Team_Avatar/";
 String label_info = "labeled_.vec";
 CascadeClassifier cascade;
@@ -42,7 +42,7 @@ int main( int argc, const char** argv )
             break;
             //error
         }
-        //cout << link << endl; //deb
+        
         
         frame = imread(repo_dir + link, 1 );
         writePrediction(frame, link);
@@ -54,22 +54,36 @@ int main( int argc, const char** argv )
 
 void writePrediction(Mat frame, String link) {
     vector<Rect> bb;
-    Mat frame_gray;
     
-    cvtColor( frame, frame_gray, CV_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
+    bool cutoff = true;
+    bool grayscale = true;
+    double cutoff_percentage = .3;
+    Mat roi = frame;
+    int cutoff_rows = 0;
+    if (cutoff) {
+        cutoff_rows = cvRound(cutoff_percentage*frame.rows);
+        rectangle(frame, Rect(0, cutoff_rows, frame.cols, frame.rows-cutoff_rows), Scalar(171, 122, 10));
+        roi = frame(Rect(0, cutoff_rows, frame.cols, frame.rows-cutoff_rows));
+    }
+    
+    Mat frame_gray;
+    frame_gray = roi;
+    if (grayscale) {
+        cvtColor( roi, frame_gray, CV_BGR2GRAY );
+        equalizeHist( frame_gray, frame_gray );
+    }
     cascade.detectMultiScale( frame_gray, bb, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
     
     ostringstream ss;
-    for( size_t i = 0; i < bb.size() && i < numDrawn; i++ )
-    {
+    for ( size_t i = 0; i < bb.size() && i < numDrawn; i++ ) {
+        bb[i].y += cutoff_rows;
         ss << " " << bb[i].x << " " << bb[i].y << " " << bb[i].width << " " << bb[i].height;
         //color is pinkish - BGR format
         rectangle(frame, bb[i], Scalar(171, 122, 234));
         imshow(window_name, frame);
+        //waitKey(10); //deb
     }
-    // if exist predictions
-    if (ss.str() != "") {
-        cout << ss.str() << endl;
-    }
+    // if no predictions, indicate so
+    cout << link; //path to img
+    cout << ss.str() << endl;
 }
