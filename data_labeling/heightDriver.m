2471ebd8850531da4b5c6534f051371218e514ad
Sam Brodkey <sambrodkey@gmail.com>
2015-04-08 02:02:34 -0400

    added code to find h'=f(d')

---
+++ data_labeling/heightDriver.m
@@ -0,0 +1,3 @@
+load('boxes_tommy1_labeled');
+pictureData = addHeightFeature(pictureData);
+plotHeightFeature(pictureData);
---
+++ data_labeling/plotHeightFeature.m
@@ -0,0 +1,27 @@
+function plotHeightFeature(pictureData)
+    d_tall = [];
+    h_tall = [];
+    d_sph = [];
+    h_sph = [];
+    for i = 1:size(pictureData,1)
+        d = pictureData{i,5};
+        h = pictureData{i,6};
+        numBuoys = size(d,1)/2;
+        for j = 1:numBuoys
+            if pictureData{i,4}(j) == 1
+                d_tall = [d_tall d(j)];
+                h_tall = [h_tall h(j)];
+            end
+            if pictureData{i,4}(j) == 2
+                d_sph = [d_sph d(j)];
+                h_sph = [h_sph h(j)];
+            end
+        end
+    end
+    
+    plot(d_sph, h_sph, 'b*');
+    hold on;
+    plot(d_tall, h_tall, 'r*');
+    xlabel('d');
+    ylabel('h');
+end
